a38e76f6f6420a22f9ba14a9112fd3750926befc
kamilleon <25117301+kamilleon@users.noreply.github.com>
2024-04-28 21:04:15 +0200

    fix saviour ability

--- core/src/main/java/me/delected/advancedabilities/ability/abilities/Saviour.java
+++ core/src/main/java/me/delected/advancedabilities/ability/abilities/Saviour.java
@@ -1,5 +1,6 @@
 package me.delected.advancedabilities.ability.abilities;
 
+import me.delected.advancedabilities.api.ChatUtils;
 import me.delected.advancedabilities.api.objects.ability.Ability;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,67 +15,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Saviour extends Ability implements Listener {
-    @Override
-    public String getId() {
-        return "saviour";
-    }
+	@Override
+	public String getId() {
+		return "saviour";
+	}
 
-    @Override
-    public boolean removeItem() {
-        return true;
-    }
-
-
-    @EventHandler
-    public void onPlayerDamage(EntityDamageEvent event) {
-        if (!(event.getEntity() instanceof Player) || event.isCancelled()) return;
-
-        Player player = (Player) event.getEntity();
-
-        if (!(player.getHealth() - event.getFinalDamage() <= 0)) return;
-        if (api.getAbilityManager().inCooldown(player, this)) return;
-
-        int position = getSaviourPosition(player.getInventory());
-        if (position < 0) return;
-
-        ItemStack savior = player.getInventory().getItem(position);
-
-        if (savior.getAmount()==1) {
-            player.getInventory().setItem(position, null);
-        } else {
-            savior.setAmount(savior.getAmount()-1);
-        }
-
-        event.setCancelled(true);
-        player.setHealth(4);
-        player.addPotionEffects(getEffects());
-
-        playSound(player);
-        addCooldown(player);
-    }
+	@Override
+	public boolean removeItem() {
+		return true;
+	}
 
 
+	@EventHandler
+	public void onPlayerDamage(EntityDamageEvent event) {
+		if (!(event.getEntity() instanceof Player) || event.isCancelled()) return;
 
-    public List<PotionEffect> getEffects() {
-        final List<PotionEffect> temp = new ArrayList<>();
-        final int duration = getConfig().getInt("duration");
-        for (String effects : getConfig().getStringList("effects")) {
-            final String[] split = effects.split(":");
-            temp.add(new PotionEffect(PotionEffectType.getByName(split[0]), duration*20, Integer.parseInt(split[1]), false, false));
-        }
-        return temp;
-    }
+		Player player = (Player) event.getEntity();
 
-    public int getSaviourPosition(Inventory inventory) {
+		if (!(player.getHealth() - event.getFinalDamage() <= 0)) return;
+		if (api.getAbilityManager().inCooldown(player, this)) return;
 
-        ItemStack[] contents = inventory.getContents();
+		int position = getSaviourPosition(player.getInventory());
+		if (position < 0) return;
 
-        for (int i = 0; i < contents.length; i++) {
-            if (getItem().isSimilar(contents[i])) return i;
-        }
-        return -1;
+		ItemStack savior = player.getInventory().getItem(position);
 
-    }
+		if (savior.getAmount() == 1) {
+			player.getInventory().setItem(position, null);
+		} else {
+			savior.setAmount(savior.getAmount() - 1);
+		}
+
+		event.setCancelled(true);
+		player.setHealth(10);
+		player.addPotionEffects(getEffects());
+
+		playSound(player);
+		addCooldown(player);
+		sendMessage(player);
+	}
+
+
+	public List<PotionEffect> getEffects() {
+		final List<PotionEffect> temp = new ArrayList<>();
+		final int duration = getConfig().getInt("duration");
+		for (String effects : getConfig().getStringList("effects")) {
+			final String[] split = effects.split(":");
+			temp.add(new PotionEffect(PotionEffectType.getByName(split[0]), duration * 20, Integer.parseInt(split[1]), false, false));
+		}
+		return temp;
+	}
+
+	public void sendMessage(Player player) {
+		List<String> messages = getConfig().getStringList("messages");
+		for (String message : messages) {
+			player.sendMessage(ChatUtils.colorize(message));
+		}
+	}
+
+	public int getSaviourPosition(Inventory inventory) {
+
+		ItemStack[] contents = inventory.getContents();
+
+		for (int i = 0; i < contents.length; i++) {
+			if (getItem().isSimilar(contents[i])) return i;
+		}
+		return -1;
+
+	}
 
 
 }
